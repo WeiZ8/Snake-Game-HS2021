82135bf6216eaa5bc78027bf6b383206566b062c
WeiZ8 <96200544+WeiZ8@users.noreply.github.com>
2021-12-17 01:56:49 +0100

    Update snake 16.12.2021 stand 1515.py

--- snake 16.12.2021 stand 1515.py
+++ snake 16.12.2021 stand 1515.py
@@ -1,18 +1,33 @@
-# # Snake game with additional features
+# Snake game with additional features
 
-# ## About the Project
-#
-# This project aims to build a simple and fun “Snake” game with some individualized features using the programming language Python. Snake is a bordered plane game, designed in the time of the arcade game blockade in the late 1970ies. The game design is set up to maneuver a dot, and later a line in direction of tokens. The tokens have the impact of making the line grow in length. The objective of the game is to eat as many tokens as possible. At the same time the player must not touch the walls, obstacle, or collide with the players avatar. Else the game ends and the player is attempt anew. 
-#
-# We will be using Pygame and pygame.menu to create this game. PyGame is an open-source library that is designed for making video games. It has inbuilt graphics and sound libraries. It is also beginner-friendly, and is suitable for cross-platform games.
-#
-# The game starts with a title screen where it is possible for the user to enter their name and refer to the instructions if needed. In order to start the game the player has to press “start” or press “quit” if they wish to leave the game.
-#
-# The gaming interface shows the score at the top left corner. To play the game the user navigates the snake, which is the green block, with the arrow keys. There are three additional features on the screen. First are the obstacles which colored in black and should be avoided. The other two features are the two types of “food” added to the game. Starting with the regular red food item that increases the length of the body by one block and the score by 1 point after being eaten. The other is a special item that appears randomly after 15 seconds. The yellow food block increases the length of the snake by 5 blocks but also increases the score by three extra points. A sound effect is played once a food item is eaten or when the snake collides with the obstacle, the wall or itself.
-#
-# Once you run into an obstacle, the boarder or the avatar itself, the game over screen pops up and shows the currently reached score. The player can choose to restart the game by clicking the “replay game” button or choose to exit the game by selecting “quit game”. 
+'''
+About the Project
 
-# ### Import the necessary libararies
+This project aims to build a simple and fun “Snake” game with some individualized features using the programming language Python. Snake is a bordered plane game, designed in the time of the arcade game blockade in the late 1970ies. The game design is set up to maneuver a dot, and later a line in direction of tokens. The tokens have the impact of making the line grow in length. The objective of the game is to eat as many tokens as possible. At the same time the player must not touch the walls, obstacle, or collide with the players avatar. Else the game ends and the player is attempt anew. 
+
+We will be using Pygame and pygame.menu to create this game. PyGame is an open-source library that is designed for making video games. It has inbuilt graphics and sound libraries. It is also beginner-friendly, and is suitable for cross-platform games.
+
+The game starts with a title screen where it is possible for the user to enter their name and refer to the instructions if needed. In order to start the game the player has to press “start” or press “quit” if they wish to leave the game.
+
+The gaming interface shows the score at the top left corner. To play the game the user navigates the snake, which is the green block, with the arrow keys. There are three additional features on the screen. First are the obstacles which colored in black and should be avoided. The other two features are the two types of “food” added to the game. Starting with the regular red food item that increases the length of the body by one block and the score by 1 point after being eaten. The other is a special item that appears randomly after 15 seconds. The yellow food block increases the length of the snake by 5 blocks but also increases the score by three extra points. A sound effect is played once a food item is eaten or when the snake collides with the obstacle, the wall or itself.
+
+Once you run into an obstacle, the boarder or the avatar itself, the game over screen pops up and shows the currently reached score. The player can choose to restart the game by clicking the “replay game” button or choose to exit the game by selecting “quit game”. 
+'''
+
+'''
+Prequisites
+
+The following packages are required and need to be installed:
+* pygame
+* pygame_menu
+* time
+* dateime
+* sys, random
+Please run “pip install” for each package. In case pip is not installed yet, please run "python get-pip.py" first.
+'''
+
+
+#Import the necessary libararies
 
 #pygame is a programming language library in python with which games can be programed. So first of all we do import pygame. 
 import pygame
