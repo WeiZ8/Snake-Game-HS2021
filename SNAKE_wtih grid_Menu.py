4cfa50ebd43148314bf09975946e2f66762da21e
hmkoh <95411649+hmkoh@users.noreply.github.com>
2021-12-21 15:49:55 +0100

    Update SNAKE_wtih grid_Menu.py

--- SNAKE_wtih grid_Menu.py
+++ SNAKE_wtih grid_Menu.py
@@ -3,52 +3,57 @@ import pygame
 import sys
 import random
 
-#Initialize pygame
+# Initialize pygame
 pygame.init()
-#---------[Game Settings]-----------
-#The color scheme used in Pygame is RGB i.e “Red Green Blue”.
-#If we set all these to 0’s, the color will be black and all 255’s will be white. Here we have defined a few colors.
+
+# ---------[Game Settings]-----------
+# The color scheme used in Pygame is RGB i.e “Red Green Blue”.
+# If we set all these to 0’s, the color will be black and all 255’s will be white. Here we have defined a few colors.
 BLACK = (0, 0, 0)
 DARKBLUE = (61, 90, 110)
-WHITE = (255,255,255)
+WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN1 = (25, 102, 25)
 GREEN2 = (51, 204, 51)
 GREEN3 = (233, 249, 185)
 LIGHT_PINK1 = (255, 230, 255)
 LIGHT_PINK2 = (255, 204, 255)
-GREY1 = (105,105,105)
+GREY1 = (105, 105, 105)
 GREY2 = (169, 169, 169)
+GREY3 = (211, 211, 211)
 
-
-#Fonts
+# Fonts
 font_style = pygame.font.SysFont("comicsans", 40)
+#Add background music
+pygame.mixer.music.load("Snake Game - Theme Song.mp3")
+pygame.mixer.music.play(-1)
 
-
-#display settings
-SCREEN_WIDTH = 540
-SCREEN_HEIGHT= 600
-GRID_SIZE = 30
-GRID_WIDTH = SCREEN_WIDTH/ GRID_SIZE
+# display settings
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 480
+GRID_SIZE = 20
+GRID_WIDTH = SCREEN_WIDTH / GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT / GRID_SIZE
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 
 # Global settings
 clock = pygame.time.Clock()
+#Set sounds and music
+pick_up_sound = pygame.mixer.Sound("Ding.mp3")
 
-
-#Game control settings
+# Game control settings
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
-#-----------[SNAKE CLASS]--------------
+
+# -----------[SNAKE CLASS]--------------
 
 class Snake():
     def __init__(self):
         self.length = 3
-        self.positions = [((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))]
+        self.positions = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))]
         self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
         self.color1 = GREEN1
         self.color2 = GREEN2
@@ -75,7 +80,7 @@ class Snake():
                 self.positions.pop()
 
     def reset(self):
-        self.length = 1
+        self.length = 3
         self.positions = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))]
         self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
         self.score = 0
@@ -93,8 +98,8 @@ class Snake():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            #To move the snake we will use key events from the KEYDOWN class of the Pygame library.
-            #The K_UP, K_DOWN, K_LEFT, and K_RIGHT events will cause the snake to move up, down, left, and right, respectively.
+            # To move the snake we will use key events from the KEYDOWN class of the Pygame library.
+            # The K_UP, K_DOWN, K_LEFT, and K_RIGHT events will cause the snake to move up, down, left, and right, respectively.
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     self.turn(UP)
@@ -105,7 +110,8 @@ class Snake():
                 elif event.key == pygame.K_RIGHT:
                     self.turn(RIGHT)
 
-#-----------[FOOD CLASS]--------------
+
+# -----------[FOOD CLASS]--------------
 class Food():
     def __init__(self):
         self.position = (0, 0)
@@ -121,19 +127,31 @@ class Food():
         r = pygame.Rect((self.position[0], self.position[1]), (GRID_SIZE, GRID_SIZE))
         pygame.draw.rect(surface, self.color, r)
 
+class Specialfood():
+    def __init__(self):
+        self.position = (0, 0)
+        self.color = BLACK
+        self.randomize_position()
 
+    def randomize_position(self):
+        self.position = (random.randint(0, GRID_WIDTH - 1) * GRID_SIZE,
+                         random.randint(0, GRID_HEIGHT - 1) * GRID_SIZE)
+
+    def draw_Specialfood(self, surface):
+        r = pygame.Rect((self.position[0], self.position[1]), (GRID_SIZE, GRID_SIZE))
+        pygame.draw.rect(surface, self.color, r)
 
 def drawGrid(surface):
     for y in range(0, int(GRID_HEIGHT)):
         for x in range(0, int(GRID_WIDTH)):
             if (x + y) % 2 == 0:
                 r = pygame.Rect((x * GRID_SIZE, y * GRID_SIZE), (GRID_SIZE, GRID_SIZE))
-                pygame.draw.rect(surface, LIGHT_PINK1, r)
+                pygame.draw.rect(surface, WHITE, r)
             else:
                 rr = pygame.Rect((x * GRID_SIZE, y * GRID_SIZE), (GRID_SIZE, GRID_SIZE))
-                pygame.draw.rect(surface, LIGHT_PINK2, rr)
+                pygame.draw.rect(surface, GREY3, rr)
 
-
+# -----------[MAIN GAME CLASS]--------------
 def main():
     pygame.init()
     # Initialise game window
@@ -147,6 +165,7 @@ def main():
 
     snake = Snake()
     food = Food()
+    specialfood = Specialfood()
 
     score_font = pygame.font.SysFont("comicsans", 30)
 
@@ -155,12 +174,22 @@ def main():
         snake.handle_keys()
         drawGrid(surface)
         snake.move()
+        # Check for collisions with normal food
         if snake.get_head_position() == food.position:
             snake.length += 1
             snake.score += 1
+            pick_up_sound.play() #sound integration
             food.randomize_position()
+        # Check for collisions with normal special food
+        if snake.get_head_position() == specialfood.position:
+            snake.length += 2
+            snake.score += 5
+            pick_up_sound.play() #sound integration
+            specialfood.randomize_position()
+
         snake.draw(surface)
         food.draw_Food(surface)
+        specialfood.draw_Specialfood(surface)
         screen.blit(surface, (0, 0))
         # We are adding a starting score
         your_score = score_font.render("Your Score: {0}".format(snake.score), 1, DARKBLUE)
@@ -168,69 +197,69 @@ def main():
         pygame.display.update()
 
 
-
 # We now create a small function to help us expedite the process of writing text for the menus
-    # We can get the dimensions of the rendered text image using text.get_rect(), which returns a Rect object with width and height attributes, to center the text respective to the window 
+# We can get the dimensions of the rendered text image using text.get_rect(), which returns a Rect object with width and height attributes, to center the text respective to the window
 
 def center_text(text, font, color, surface, x, y):
     textobj = font.render(text, 1, color)
-    textrect = textobj.get_rect(center=(SCREEN_WIDTH/2, y))
+    textrect = textobj.get_rect(center=(SCREEN_WIDTH / 2, y))
     surface.blit(textobj, textrect)
 
-def left_text (text, font, color, surface, x, y):
+
+def left_text(text, font, color, surface, x, y):
     textobj = font.render(text, 1, color)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
 
-def button_text (text, font, color, surface, x, y):
+
+def button_text(text, font, color, surface, x, y):
     textobj = font.render(text, 1, color)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
-    
-#---------------------[MENU]------------------------
-#neww
+
+# ---------------------[MENU]------------------------
+# neww
 def main_menu():
     while True:
- 
-        screen.fill((0,0,0))
-        center_text('Main menu', font_style, (255, 255, 255), screen, 40, 40)
- 
-        mx, my = pygame.mouse.get_pos()
- 
 
- # Creating the buttons
- # We use the virtual property of rect and assign its center to the middle of the screen
-        
+        screen.fill((0, 0, 0))
+        center_text('Main menu', font_style, (255, 255, 255), screen, 40, 40)
+
+        mx, my = pygame.mouse.get_pos()
+
+        # Creating the buttons
+        # We use the virtual property of rect and assign its center to the middle of the screen
+
         button_1 = pygame.Rect(0, 0, 200, 75)
-        button_1.center = ((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
-        pygame.draw.rect(screen, (255,255,255), button_1)
-    
+        button_1.center = ((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+        pygame.draw.rect(screen, (255, 255, 255), button_1)
+
         button_2 = pygame.Rect(0, 0, 200, 75)
-        button_2.centerx = ((SCREEN_WIDTH/2))
-        button_2.centery = ((button_1.centery)+button_1.height*1.5)  #We distanciate the second button by the lenght of exaclty 0.5 button
+        button_2.centerx = ((SCREEN_WIDTH / 2))
+        button_2.centery = ((
+                                button_1.centery) + button_1.height * 1.5)  # We distanciate the second button by the lenght of exaclty 0.5 button
 
         pygame.draw.rect(screen, (255, 0, 0), button_1)
         pygame.draw.rect(screen, (255, 0, 0), button_2)
-        
+
         if button_1.collidepoint((mx, my)):
             if click:
                 main()
         if button_2.collidepoint((mx, my)):
             if click:
                 options()
-        
-        #Adding text to the buttons 
+
+        # Adding text to the buttons
         # We use True in "font_style.render", to activate antialiasing and make the text smoother
         Text_button_1 = font_style.render("Play", True, 20)
-        text_rect_1 = Text_button_1.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+        text_rect_1 = Text_button_1.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
         screen.blit(Text_button_1, text_rect_1)
 
         Text_button_2 = font_style.render("Instructions", True, 20)
-        text_rect_2 = Text_button_2.get_rect(center=(SCREEN_WIDTH/2, button_2.centery))
+        text_rect_2 = Text_button_2.get_rect(center=(SCREEN_WIDTH / 2, button_2.centery))
         screen.blit(Text_button_2, text_rect_2)
-
 
         click = False
         for event in pygame.event.get():
@@ -244,34 +273,38 @@ def main_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
- 
+
         pygame.display.update()
         clock.tick(30)
+
 
 def options():
     running = True
     while running:
-        screen.fill((0,0,0))
+        screen.fill((0, 0, 0))
         mx, my = pygame.mouse.get_pos()
-#Text for options menu
+        # Text for options menu
         Title = center_text('Options', font_style, (255, 255, 255), screen, 20, 20)
-        Info_1 = center_text('Eat fruit to get longer', font_style, (255, 255, 255), screen, SCREEN_WIDTH/2, SCREEN_HEIGHT-30*12)
-        Info_2 = center_text('One point', font_style, (255, 255, 255), screen, SCREEN_WIDTH/2, SCREEN_HEIGHT-30*10)
-        Info_3 = center_text('Five points', font_style, (255, 255, 255), screen, SCREEN_WIDTH/2, SCREEN_HEIGHT-30*8)
+        Info_1 = center_text('Eat fruit to get longer', font_style, (255, 255, 255), screen, SCREEN_WIDTH / 2,
+                             SCREEN_HEIGHT - 30 * 12)
+        Info_2 = center_text('One point', font_style, (255, 255, 255), screen, SCREEN_WIDTH / 2,
+                             SCREEN_HEIGHT - 30 * 10)
+        Info_3 = center_text('Five points', font_style, (255, 255, 255), screen, SCREEN_WIDTH / 2,
+                             SCREEN_HEIGHT - 30 * 8)
 
-#Images/symbols describing the text
+        # Images/symbols describing the text
 
         normal_food_sym = pygame.Rect(0, 0, 25, 25)
-        normal_food_sym.midleft = 100, SCREEN_HEIGHT-30*10
+        normal_food_sym.midleft = 100, SCREEN_HEIGHT - 30 * 10
         pygame.draw.rect(screen, RED, normal_food_sym)
 
         special_food_sym = pygame.Rect(0, 0, 25, 25)
-        special_food_sym.midleft = 100, SCREEN_HEIGHT-30*8
+        special_food_sym.midleft = 100, SCREEN_HEIGHT - 30 * 8
         pygame.draw.rect(screen, GREEN1, special_food_sym)
 
-# Back button 
+        # Back button
         Back_button = pygame.Rect(0, 0, 150, 50)
-        Back_button.center=(SCREEN_WIDTH/2, SCREEN_HEIGHT-30*2)
+        Back_button.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT - 30 * 2)
         pygame.draw.rect(screen, (GREY1), Back_button)
 
         Back_button_text = font_style.render("Back", True, 10)
@@ -281,7 +314,6 @@ def options():
         if Back_button.collidepoint((mx, my)):
             if click:
                 main_menu()
-
 
         click = False
         for event in pygame.event.get():
@@ -294,9 +326,10 @@ def options():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
-        
+
         pygame.display.update()
         clock.tick(30)
 
 
 main_menu()
+
